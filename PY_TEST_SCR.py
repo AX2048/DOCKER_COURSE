61ab34ec0a15e5974dac698d16eb7083494feaa3
Alex <ax.dev@yahoo.com>
2023-02-27 16:32:45 +0300

    monday beggin from fix

--- PY_TEST_SCR.py
+++ PY_TEST_SCR.py
@@ -5,7 +5,7 @@ import platform
 from datetime import datetime
 from random import randint
 import socket
-#from pip import _internal
+
 
 print(f'| Current DT :: {datetime.now()} |')
 print(f'| sys.v :: {sys.version} |')
@@ -14,4 +14,18 @@ print(f'| Executable :: {os.path.dirname(sys.executable), sys.executable} |')
 print(f'| randint :: {randint(0, 100)} |')
 print(f'| User @ Host :: {os.getlogin()}@{socket.gethostname()} |')
 
-#print(os.system('ip a'))
+print('')
+
+# Get the operating system name
+os_name = platform.system()
+
+# Get the operating system version
+os_version = platform.release()
+
+# Get the operating system architecture
+os_architecture = platform.machine()
+
+# Print the OS information
+print(f"| Operating System: {os_name} |")
+print(f"| Version: {os_version} |")
+print(f"| Architecture: {os_architecture} |")
--- python-app/main.py
+++ python-app/main.py
@@ -5,8 +5,7 @@ import sys
 import platform
 from datetime import datetime
 from random import randint
-import socket
-#from pip import _internal
+#import socket
 
 print('Добро пожаловать в супер календарь')
 
@@ -17,16 +16,10 @@ print(calendar.month(year, month))
 
 print('Всего хорошего!')
 
-
-
-
-current_datetime = datetime.now()
-r = randint(0, 25)
-
-
+print(f'| Current DT :: {datetime.now()} |')
 print(f'| sys.v :: {sys.version} |')
 print(f'| platform.v :: {platform.python_version()} |')
 print(f'| Executable :: {os.path.dirname(sys.executable), sys.executable} |')
-print(f'| randint :: {r} |')
+print(f'| randint :: {randint(0, 100)} |')
 #print(f'| User @ Host :: {os.getlogin()}@{socket.gethostname()} |')
-# use docker run -e LOGNAME -it my-calendar:4.1.0 for os.getlogin()
+#use docker run -e LOGNAME -it my-calendar:4.1.0 for os.getlogin()
